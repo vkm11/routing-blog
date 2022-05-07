634b1c68676c539c38beed107eb8d0f99eee7bd8
vijaykumar <vijay1111mane@gmail.com>
2022-05-07 14:36:08 +0530

    commit;

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutCompanyComponent } from './about-company/about-company.component';
+import { AboutMeComponent } from './about-me/about-me.component';
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 import { NoPageComponent } from './no-page/no-page.component';
@@ -7,8 +9,14 @@ import { UserComponent } from './user/user.component';
 
 const routes: Routes = [
   // {path: "home", component:HomeComponent},
-  {path: "about", component:AboutComponent},
-  {path: "user/:id", component:UserComponent},
+  { 
+    component:AboutComponent,
+    path: "about", children:[
+      { path: "company", component:AboutCompanyComponent },
+      { path: "me", component:AboutMeComponent }
+    ]  
+  },
+  { path: "user/:id", component:UserComponent },
   { component:HomeComponent, path: ""},
   { component:NoPageComponent, path: "**"}
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { UserComponent } from './user/user.component';
 import { NoPageComponent } from './no-page/no-page.component';
+import { AboutCompanyComponent } from './about-company/about-company.component';
+import { AboutMeComponent } from './about-me/about-me.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,9 @@ import { NoPageComponent } from './no-page/no-page.component';
     HomeComponent,
     AboutComponent,
     UserComponent,
-    NoPageComponent
+    NoPageComponent,
+    AboutCompanyComponent,
+    AboutMeComponent
   ],
   imports: [
     BrowserModule,
