63bcef8cc606d4c52088d0d2a4eb034eabad4398
vijaykumar <vijay1111mane@gmail.com>
2022-05-07 13:19:34 +0530

    commit;

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
+import { NoPageComponent } from './no-page/no-page.component';
 import { UserComponent } from './user/user.component';
 
 const routes: Routes = [
-  {path: "home", component:HomeComponent},
+  // {path: "home", component:HomeComponent},
   {path: "about", component:AboutComponent},
   {path: "user/:id", component:UserComponent},
-  {path: "", component:HomeComponent}
+  { component:HomeComponent, path: ""},
+  { component:NoPageComponent, path: "**"}
 ];
 
 @NgModule({
